c0146add5f265f53ec40d119f0ba26e844cdbec9
Luis Gabriel Rodriguez Alejos <lralejos@riafinancial.com>
2018-06-04 15:10:17 +0200

    green Sets a value to an attribute

--- lab/spec/nanoSpec.js
+++ lab/spec/nanoSpec.js
@@ -1,10 +1,19 @@
 describe('Nano JS', function(){
+    const elementId = 'a';
+    beforeEach(function(){
+        document.getElementById(elementId).removeAttribute("style")
+    });
     it('Applyies css style', function(){
-        const elementId = 'a';
         const style = 'background-color: rgb(34, 34, 34);';
 
         $('#' + elementId).css(style);
         
         expect(document.getElementById(elementId).style.cssText).toBe(style);
+    });
+
+    it("Sets a value to an attribute", function(){
+        $('#' + elementId).attr("class", "on");
+
+        expect(document.getElementById(elementId).outerHTML).toContain('class="on"');
     });
 });
