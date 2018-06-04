08bc9acd4b618d0ba9466ebc02e3cc096f158184
Luis Gabriel Rodriguez Alejos <lralejos@riafinancial.com>
2018-06-04 14:44:54 +0200

    green Applyies css style

---
+++ lab/spec/nanoSpec.js
@@ -0,0 +1,9 @@
+describe("Nano JS", function(){
+    it("Applyies css style", function(){
+        const style = 'background-color: rgb(34, 34, 34);';
+        
+        $("#a").css(style);
+        
+        expect(document.getElementById("a").style.cssText).toBe(style);
+    });
+});
